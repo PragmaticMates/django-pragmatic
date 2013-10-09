6cc5808b20ddf3b69c499b5bba8b6bb90ae2b05a
Erik Telepovsky <erik.telepovsky@gmail.com>
2013-10-09 13:03:23 +0200

    pragmatic.py > pragmatic_tags.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-pragmatic',
-    version='0.1.1',
+    version='0.1.2',
     description='Pragmatic tools and utilities for Django projects',
     long_description=open('README').read(),
     author='Pragmatic Mates',
