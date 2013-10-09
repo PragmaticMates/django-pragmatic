3dad7ce6b470079b48a86a4385bdc5775de64751
Erik Telepovsky <erik.telepovsky@gmail.com>
2013-10-09 14:58:59 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-pragmatic',
-    version='0.2.0',
+    version='0.2.1',
     description='Pragmatic tools and utilities for Django projects',
     long_description=open('README').read(),
     author='Pragmatic Mates',
