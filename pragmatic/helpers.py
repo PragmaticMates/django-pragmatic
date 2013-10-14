1c4aecf7f19b85b8b0bfce93ed819593f3d75d09
Erik Telepovsky <erik.telepovsky@gmail.com>
2013-10-14 10:15:57 +0200

    round_to_n_decimal_places

--- pragmatic/helpers.py
+++ pragmatic/helpers.py
@@ -2,3 +2,8 @@ def base64_to_file(content, filepath):
     fh = open(filepath, "wb")
     fh.write(content.decode('base64'))
     fh.close()
+
+
+def round_to_n_decimal_places(self, value, n):
+    power = 10 ** n
+    return float(int(value * power)) / power
