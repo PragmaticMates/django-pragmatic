bf12bc364ed9a3bd8a3a342531e6ac533933cd7b
Erik Telepovský <erik.telepovsky@gmail.com>
2016-11-09 14:40:08 +0100

    sudo in travis, dropping support of python 2.5

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     install_requires=('django-filter', 'django', 'python-pragmatic'),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Operating System :: OS Independent',
