17109ecc6475183c91a8f4271f52f04ac93d4690
Erik Telepovský <erik.telepovsky@gmail.com>
2018-03-15 15:13:18 +0100

    MaintenanceModeMiddleware improved

--- pragmatic/middleware.py
+++ pragmatic/middleware.py
@@ -14,16 +14,29 @@ class MaintenanceModeMiddleware(MiddlewareMixin):
     template_name = 'maintenance_mode.html'
 
     def process_response(self, request, response):
-        if getattr(settings, 'MAINTENANCE_MODE', False):
-            try:
-                context_processors = settings.TEMPLATE_CONTEXT_PROCESSORS
-            except AttributeError:
-                context_processors = settings.TEMPLATES[0]['OPTIONS']['context_processors']
+        # don't show maintenance mode screen if not required
+        if not getattr(settings, 'MAINTENANCE_MODE', False):
+            return response
 
-            if 'django.core.context_processors.request' in context_processors or \
-                'django.template.context_processors.request' in context_processors:
-                template = loader.get_template(self.template_name)
-                return HttpResponse(template.render({}, request))
-            else:
-                return SimpleTemplateResponse(self.template_name).render()
-        return response
+        # Check if logged in user is staff
+        try:
+            is_staff = request.user.is_authenticated and request.user.is_staff
+        except AttributeError:
+            is_staff = False
+
+        # bypass maintenance mode if staff user is logged in
+        if is_staff:
+            return response
+
+        # render maintenance mode screen template
+        try:
+            context_processors = settings.TEMPLATE_CONTEXT_PROCESSORS
+        except AttributeError:
+            context_processors = settings.TEMPLATES[0]['OPTIONS']['context_processors']
+
+        if 'django.core.context_processors.request' in context_processors or \
+            'django.template.context_processors.request' in context_processors:
+            template = loader.get_template(self.template_name)
+            return HttpResponse(template.render({}, request))
+        else:
+            return SimpleTemplateResponse(self.template_name).render()
