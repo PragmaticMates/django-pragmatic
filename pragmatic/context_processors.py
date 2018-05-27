48e1fd738fe65d63d640f51325161269f755afdf
Erik Telepovský <erik.telepovsky@gmail.com>
2018-05-27 20:16:01 +0200

    url_identifier and settings context processors

--- pragmatic/context_processors.py
+++ pragmatic/context_processors.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.conf import settings as django_settings
 
 
 def date_formats(request):
@@ -6,13 +6,40 @@ def date_formats(request):
     Returns a lazy 'date formats' context variables.
     """
     return {
-        'DATE_FORMAT_JS': settings.DATE_FORMAT_JS,
-        'DATE_FORMAT_TAG': settings.DATE_FORMAT_TAG,
-        'DATE_FORMAT_FULLMONTH_TAG': settings.DATE_FORMAT_FULLMONTH_TAG
+        'DATE_FORMAT_JS': django_settings.DATE_FORMAT_JS,
+        'DATE_FORMAT_TAG': django_settings.DATE_FORMAT_TAG,
+        'DATE_FORMAT_FULLMONTH_TAG': django_settings.DATE_FORMAT_FULLMONTH_TAG
     }
 
 
 def installed_apps(request):
     return {
-        'INSTALLED_APPS': settings.INSTALLED_APPS
+        'INSTALLED_APPS': django_settings.INSTALLED_APPS
+    }
+
+
+def url_identifier(request):
+    try:
+        url_name = request.resolver_match.url_name
+    except AttributeError:
+        return {}
+
+    try:
+        namespace = request.resolver_match.namespaces[0]
+        return {
+            'url_namespace': namespace,
+            'url_name': url_name,
+            'url_id': namespace + ':' + url_name
+        }
+    except IndexError:
+        return {
+            'url_id': url_name
+        }
+
+
+def settings(request):
+    import django
+
+    return {
+        'settings': django_settings
     }
