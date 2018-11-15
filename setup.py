446b82cca81fad84c1e3c9b1d62366c45ba16c2e
Erik Telepovský <erik.telepovsky@gmail.com>
2018-11-15 14:39:30 +0100

    v2.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-pragmatic',
-    version='2.7.2',
+    version='2.8.0',
     description='Pragmatic tools and utilities for Django projects',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',
