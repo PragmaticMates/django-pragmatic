b4e9653814f405f3ae3cf84c1f215be7c6d03247
rsoltys <radovan.soltys@gmail.com>
2020-04-17 09:26:34 +0200

    date-and-time-picker.js

---
+++ pragmatic/static/pragmatic/js/date-and-time-picker.js
@@ -0,0 +1,66 @@
+jQuery(document).ready(function ($) {
+    'use strict';
+    init_date_and_time_pickers();
+});
+
+function init_date_and_time_pickers() {
+    function getKeyBinds(input) {
+        return {
+            left: function (widget) {
+                var selectionStart = input[0].selectionStart;
+                if (selectionStart > 0) {
+                    input[0].selectionStart = selectionStart - 1;
+                    input[0].selectionEnd = input[0].selectionStart;
+                }
+            },
+            right: function (widget) {
+                var selectionStart = input[0].selectionStart;
+                if (selectionStart < input[0].value.length) {
+                    input[0].selectionStart = selectionStart + 1;
+                    input[0].selectionEnd = input[0].selectionStart;
+                }
+            }
+        };
+    }
+
+    const commonSettings = {
+        debug: true,
+        focusOnShow: false,
+        keyBinds: getKeyBinds($(this)),
+        icons: {
+            time: 'fa fa-clock-o',
+            date: 'fa fa-calendar',
+            up: 'fa fa-chevron-up',
+            down: 'fa fa-chevron-down',
+            previous: 'fa fa-chevron-left',
+            next: 'fa fa-chevron-right',
+            today: 'fa fa-dot-circle-o',
+            clear: 'fa fa-trash',
+            close: 'fa fa-times'
+        }
+    }
+
+    $('.datetime-picker, [name$="datetime"], .datetimeinput').each(function () {
+        $(this).attr('autocomplete', 'off');
+        $(this).datetimepicker({
+            format: 'DD.MM.YYYY HH:mm',
+            ...commonSettings
+        });
+    });
+
+    $('.date-picker, .dateinput').each(function () {
+        $(this).attr('autocomplete', 'off');
+        $(this).datetimepicker({
+            format: 'DD.MM.YYYY',
+            ...commonSettings
+        });
+    });
+
+    $('.time-picker').each(function () {
+        $(this).attr('autocomplete', 'off');
+        $(this).datetimepicker({
+            format: 'HH:mm',
+            ...commonSettings
+        });
+    });
+}
