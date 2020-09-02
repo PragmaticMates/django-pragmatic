f2297980aa2ac367aeb3294489cedce0dc18b6f1
rsoltys <radovan.soltys@gmail.com>
2020-09-02 16:19:25 +0200

    class_for_name

---
+++ pragmatic/utils.py
@@ -0,0 +1,9 @@
+import importlib
+
+
+def class_for_name(module_name, class_name):
+    # load the module, will raise ImportError if module cannot be loaded
+    m = importlib.import_module(module_name)
+    # get the class, will raise AttributeError if class cannot be found
+    c = getattr(m, class_name)
+    return c
