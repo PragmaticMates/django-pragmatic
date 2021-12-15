e78a1a697cb2258d79f8e18c139a7d32c455ef2d
eriktelepovsky <erik.telepovsky@gmail.com>
2021-12-15 14:06:45 +0100

    3.6.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-pragmatic',
-    version='3.5.0',
+    version='3.6.0',
     description='Pragmatic tools and utilities for Django projects',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',
