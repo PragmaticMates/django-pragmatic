6777704d1acb66faed3cac833a1219595807e903
eriktelepovsky <erik.telepovsky@gmail.com>
2022-01-25 14:55:59 +0100

    serializers.ContentTypeNaturalField

--- pragmatic/serializers.py
+++ pragmatic/serializers.py
@@ -10,7 +10,11 @@ class ContentTypeSerializer(serializers.ModelSerializer):
         fields = ['id', 'app_label', 'model']
 
     def to_internal_value(self, data):
-        app_label, model = data.split('.')
+        if isinstance(data, dict):
+            app_label = data['app_label']
+            model = data['model']
+        else:
+            app_label, model = data.split('.')
 
         content_type = ContentType.objects.get_by_natural_key(app_label, model)
 
@@ -22,3 +26,14 @@ class ContentTypeSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         return '.'.join(instance.natural_key())
+
+
+class ContentTypeNaturalField(serializers.PrimaryKeyRelatedField):
+    def use_pk_only_optimization(self):
+        return False
+
+    def to_internal_value(self, data):
+        return ContentType.objects.get_by_natural_key(*data.split('.'))
+
+    def to_representation(self, value):
+        return '.'.join(value.natural_key())
