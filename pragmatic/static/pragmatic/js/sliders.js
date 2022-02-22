4f1b3fb9f9e5cb87cd9d5ba13757a5b91292b4ee
rsoltys <radovan.soltys@gmail.com>
2022-02-22 10:17:36 +0100

    Slider - allow to init slider manually by selector

--- pragmatic/static/pragmatic/js/sliders.js
+++ pragmatic/static/pragmatic/js/sliders.js
@@ -7,166 +7,166 @@ function init_sliders() {
     $('[data-slider]').each(function () {
         init_slider($(this));
     });
+}
 
-    function init_slider(slider_element) {
-        var min = slider_element.attr('data-slider-min');
-        var max = slider_element.attr('data-slider-max');
-        var useInput = slider_element.attr('data-slider-input');
-        useInput = typeof useInput === typeof undefined || useInput === 'true';
+function init_slider(slider_element) {
+    var min = slider_element.attr('data-slider-min');
+    var max = slider_element.attr('data-slider-max');
+    var useInput = slider_element.attr('data-slider-input');
+    useInput = typeof useInput === typeof undefined || useInput === 'true';
 
-        var slider = slider_element.slider({
-            formatter: function (val) {
-                if (Array.isArray(val)) {
-                    return ((val[0] <= min) ? "-" : val[0]) + " : " + val[1];
-                } else {
-                    return (val <= min) ? "-" : val;
-                }
+    var slider = slider_element.slider({
+        formatter: function (val) {
+            if (Array.isArray(val)) {
+                return ((val[0] <= min) ? "-" : val[0]) + " : " + val[1];
+            } else {
+                return (val <= min) ? "-" : val;
             }
-        });
+        }
+    });
 
-        function initLabels(value) {
-            if (useInput === true) {
-                var inputAfterWrapper = $('<div class="input-group input-group-sm"><input type="text" class="form-control slider-after"></div>');
-                inputAfterWrapper.insertAfter(sliderElement);
-                var inputAfter = inputAfterWrapper.find('input');
+    function initLabels(value) {
+        if (useInput === true) {
+            var inputAfterWrapper = $('<div class="input-group input-group-sm"><input type="text" class="form-control slider-after"></div>');
+            inputAfterWrapper.insertAfter(sliderElement);
+            var inputAfter = inputAfterWrapper.find('input');
+
+            if (valueAfter) {
+                var afterValueWrapper = $('<div class="input-group-append"><span class="input-group-text">' + valueAfter + '</span></div>');
+                inputAfterWrapper.append(afterValueWrapper)
+            }
+
+            if (value.length > 1) {
+                var inputBeforeWrapper = $('<div class="input-group input-group-sm"><input type="text" class="form-control slider-before"></div>');
+                inputBeforeWrapper.insertBefore(sliderElement);
+                var inputBefore = inputBeforeWrapper.find('input');
 
                 if (valueAfter) {
-                    var afterValueWrapper = $('<div class="input-group-append"><span class="input-group-text">' + valueAfter + '</span></div>');
-                    inputAfterWrapper.append(afterValueWrapper)
+                    inputBeforeWrapper.append(afterValueWrapper.clone())
                 }
 
-                if (value.length > 1) {
-                    var inputBeforeWrapper = $('<div class="input-group input-group-sm"><input type="text" class="form-control slider-before"></div>');
-                    inputBeforeWrapper.insertBefore(sliderElement);
-                    var inputBefore = inputBeforeWrapper.find('input');
+                inputBefore.keyup(function () {
+                    setValueByInput(inputBefore, inputAfter);
+                });
 
-                    if (valueAfter) {
-                        inputBeforeWrapper.append(afterValueWrapper.clone())
-                    }
+                inputAfter.keyup(function () {
+                    setValueByInput(inputBefore, inputAfter);
+                });
 
-                    inputBefore.keyup(function () {
-                        setValueByInput(inputBefore, inputAfter);
-                    });
+                inputBefore.change(function () {
+                    updateValidInputValueOnChange(inputBefore, inputAfter);
+                });
 
-                    inputAfter.keyup(function () {
-                        setValueByInput(inputBefore, inputAfter);
-                    });
-
-                    inputBefore.change(function () {
-                        updateValidInputValueOnChange(inputBefore, inputAfter);
-                    });
-
-                    inputAfter.change(function () {
-                        updateValidInputValueOnChange(inputBefore, inputAfter);
-                    });
-                } else {
-                    inputAfter.keyup(function () {
-                        setValueByInput(null, inputAfter);
-                    });
-
-                    inputAfter.change(function () {
-                        updateValidInputValueOnChange(null, inputAfter)
-                    });
-                }
+                inputAfter.change(function () {
+                    updateValidInputValueOnChange(inputBefore, inputAfter);
+                });
             } else {
-                $('<div class="label slider-after"></div>').insertAfter(sliderElement);
+                inputAfter.keyup(function () {
+                    setValueByInput(null, inputAfter);
+                });
 
-                if (value.length > 1) {
-                    $('<div class="label slider-before"></div>').insertBefore(sliderElement);
-                }
+                inputAfter.change(function () {
+                    updateValidInputValueOnChange(null, inputAfter)
+                });
+            }
+        } else {
+            $('<div class="label slider-after"></div>').insertAfter(sliderElement);
+
+            if (value.length > 1) {
+                $('<div class="label slider-before"></div>').insertBefore(sliderElement);
             }
         }
+    }
 
-        function getValidValue(value) {
-            if (isNaN(value) || Number(value) < Number(min)) {
-                return min;
-            }
-
-            if (Number(value) > Number(max)) {
-                return max;
-            }
-
-            return value;
+    function getValidValue(value) {
+        if (isNaN(value) || Number(value) < Number(min)) {
+            return min;
         }
 
-        function updateLabels() {
-            var values = slider.slider('getValue');
+        if (Number(value) > Number(max)) {
+            return max;
+        }
 
-            if (values.length > 1) {
-                if (values[0] <= min) {
-                    values[0] = '-'
-                }
+        return value;
+    }
 
-                if (useInput === true){
-                    parent.find('.slider-before').val(values[0]);
-                    parent.find('.slider-after').val(values[1]);
-                } else {
-                    parent.find('.slider-before').html(values[0] + ' ' + valueAfter);
-                    parent.find('.slider-after').html(values[1] + ' ' + valueAfter);
-                }
+    function updateLabels() {
+        var values = slider.slider('getValue');
+
+        if (values.length > 1) {
+            if (values[0] <= min) {
+                values[0] = '-'
+            }
+
+            if (useInput === true){
+                parent.find('.slider-before').val(values[0]);
+                parent.find('.slider-after').val(values[1]);
             } else {
-                if (values <= min) {
-                    values = '-'
-                }
+                parent.find('.slider-before').html(values[0] + ' ' + valueAfter);
+                parent.find('.slider-after').html(values[1] + ' ' + valueAfter);
+            }
+        } else {
+            if (values <= min) {
+                values = '-'
+            }
 
-                if (useInput === true) {
-                    parent.find('.slider-after').val(values);
-                } else {
-                    parent.find('.slider-after').html(values + ' ' + valueAfter);
-                }
+            if (useInput === true) {
+                parent.find('.slider-after').val(values);
+            } else {
+                parent.find('.slider-after').html(values + ' ' + valueAfter);
             }
         }
+    }
 
-        function updateValidInputValueOnChange(inputBefore, inputAfter) {
-            var afterValue = getValidValue(inputAfter.val());
-            if (inputBefore) {
-                var beforeValue = getValidValue(inputBefore.val());
-                if (Number(beforeValue) > Number(afterValue)) {
-                    inputAfter.val(beforeValue);
-                    inputBefore.val(afterValue);
-                } else {
-                    inputAfter.val(afterValue);
-                    inputBefore.val(beforeValue);
-                }
+    function updateValidInputValueOnChange(inputBefore, inputAfter) {
+        var afterValue = getValidValue(inputAfter.val());
+        if (inputBefore) {
+            var beforeValue = getValidValue(inputBefore.val());
+            if (Number(beforeValue) > Number(afterValue)) {
+                inputAfter.val(beforeValue);
+                inputBefore.val(afterValue);
             } else {
                 inputAfter.val(afterValue);
+                inputBefore.val(beforeValue);
             }
+        } else {
+            inputAfter.val(afterValue);
+        }
+    }
+
+    function setValueByInput(inputBefore, inputAfter) {
+        var afterValue = getValidValue(inputAfter.val());
+        if (inputBefore) {
+            var beforeValue = getValidValue(inputBefore.val());
+            try {
+                var val = JSON.parse("[" + beforeValue + ',' + afterValue + "]");
+                slider.slider('setValue', val);
+            } catch (e) {
+                // silently ignore
+            }
+
+        } else {
+            slider.slider('setValue', getValidValue(inputAfter.val()));
+        }
+    }
+
+    if (slider_element && slider_element.attr('data-slider-show-value')) {
+        var parent = slider_element.parent();
+        var valueAfter = slider_element.attr('data-slider-value-after');
+        var sliderElement = slider_element.slider('getElement');
+        var value = slider_element.slider('getValue');
+
+        parent.addClass('slider-wrapper');
+
+        if (valueAfter == null) {
+            valueAfter = '';
         }
 
-        function setValueByInput(inputBefore, inputAfter) {
-            var afterValue = getValidValue(inputAfter.val());
-            if (inputBefore) {
-                var beforeValue = getValidValue(inputBefore.val());
-                try {
-                    var val = JSON.parse("[" + beforeValue + ',' + afterValue + "]");
-                    slider.slider('setValue', val);
-                } catch (e) {
-                    // silently ignore
-                }
+        initLabels(value);
+        updateLabels();
 
-            } else {
-                slider.slider('setValue', getValidValue(inputAfter.val()));
-            }
-        }
-
-        if (slider_element && slider_element.attr('data-slider-show-value')) {
-            var parent = slider_element.parent();
-            var valueAfter = slider_element.attr('data-slider-value-after');
-            var sliderElement = slider_element.slider('getElement');
-            var value = slider_element.slider('getValue');
-
-            parent.addClass('slider-wrapper');
-
-            if (valueAfter == null) {
-                valueAfter = '';
-            }
-
-            initLabels(value);
+        slider_element.on('change', function () {
             updateLabels();
-
-            slider_element.on('change', function () {
-                updateLabels();
-            });
-        }
+        });
     }
 }
