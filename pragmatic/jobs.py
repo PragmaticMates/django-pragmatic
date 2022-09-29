912d297766ab9926af4a69f1819b160f67bf462d
eriktelepovsky <erik.telepovsky@gmail.com>
2022-09-29 15:39:00 +0200

    ConnectionClosingWorker

--- pragmatic/jobs.py
+++ pragmatic/jobs.py
@@ -1,7 +1,34 @@
 from django_rq import job
 from django.conf import settings
+from django.db import connection
+
+from rq.worker import Worker, SimpleWorker
 
 
-@job(settings.MAILS_QUEUE)
+@job(getattr(settings, 'MAILS_QUEUE', 'default'))
 def send_mail_in_background(email):
     email.send()
+
+
+class ConnectionClosingWorker(Worker):
+    """
+    RQ Worker that closes the database connection before forking.
+
+    See also https://github.com/ui/django-rq/issues/17
+    """
+
+    def execute_job(self, job, queue):
+        connection.close()
+        return super().execute_job(job, queue)
+
+
+class ConnectionClosingSimpleWorker(SimpleWorker):
+    """
+        RQ Worker that closes the database connection before forking.
+
+        See also https://github.com/ui/django-rq/issues/17
+        """
+
+    def execute_job(self, job, queue):
+        connection.close()
+        return super().execute_job(job, queue)
