46395d20138c8c7081b1b9596c919c6b51329eee
Erik Telepovský <erik.telepovsky@gmail.com>
2026-04-21 09:18:35 +0200

    ConnectionsClosingWorker and ConnectionsClosingSimpleWorker

--- pragmatic/jobs.py
+++ pragmatic/jobs.py
@@ -1,6 +1,6 @@
 from django_rq import job
 from django.conf import settings
-from django.db import connection
+from django.db import connection, connections
 
 from rq.worker import Worker, SimpleWorker
 
@@ -24,11 +24,28 @@ class ConnectionClosingWorker(Worker):
 
 class ConnectionClosingSimpleWorker(SimpleWorker):
     """
-        RQ Worker that closes the database connection before forking.
-
-        See also https://github.com/ui/django-rq/issues/17
-        """
+    Simple RQ Worker that closes the database connection before forking.
+    """
 
     def execute_job(self, job, queue):
         connection.close()
+        return super().execute_job(job, queue)
+
+
+class ConnectionsClosingWorker(Worker):
+    """
+    RQ Worker that closes all database connections before forking.
+    """
+
+    def execute_job(self, job, queue):
+        connections.close_all()
+        return super().execute_job(job, queue)
+
+
+class ConnectionsClosingSimpleWorker(SimpleWorker):
+    """
+    Simple RQ Worker that closes all database connections before forking.
+    """
+    def execute_job(self, job, queue):
+        connections.close_all()
         return super().execute_job(job, queue)
